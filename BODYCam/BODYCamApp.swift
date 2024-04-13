3ca2700e387b59a32df2b0cebe70919773510ee9
PieterNatanael <pieternatanael@icloud.com>
2024-04-13 10:02:04 +0800

    Initial Commit

---
+++ BODYCam/BODYCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  BODYCamApp.swift
+//  BODYCam
+//
+//  Created by Pieter Yoshua Natanael on 13/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BODYCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
