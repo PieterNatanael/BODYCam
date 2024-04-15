48312054dff50a4280292b03f88e7cb0747f0868
PieterNatanael <pieternatanael@icloud.com>
2024-04-15 23:01:01 +0800

    Initial Commit

---
+++ BODYCam/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  BODYCam
+//
+//  Created by Pieter Yoshua Natanael on 13/04/24.
+//
+
+import Foundation
---
+++ BODYCam/SceneDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  SceneDelegate.swift
+//  BODYCam
+//
+//  Created by Pieter Yoshua Natanael on 13/04/24.
+//
+
+import Foundation
